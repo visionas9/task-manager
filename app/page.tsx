b388f28a34b1860ba9db68b93ccf843b0c914065
Alperen Sirli <alperensirli99@hotmail.com>
2026-05-02 18:32:56 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center font-sans">
+      <h1>Task Manager</h1>
+    </div>
+  );
+}
