45ccd19b3a0ac5afd7b696ed757eb36ab366fabd
Alperen Sirli <alperensirli99@hotmail.com>
2026-05-03 18:58:16 +0200

    task page redicrect route based on today's id and formatted date function logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-export default function Home() {}
+import { redirect } from "next/navigation";
+import { formattedDate } from "./lib/utils";
+
+export default function Home() {
+  const id = formattedDate();
+
+  return redirect(`/task/${id}`);
+}
