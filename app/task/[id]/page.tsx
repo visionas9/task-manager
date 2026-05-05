b4619977353f426de432e76e0339a0e9a662b3f6
Alperen Sirli <alperensirli99@hotmail.com>
2026-05-05 22:21:19 +0200

    I will need id state in task id route not in task card component

--- app/task/[id]/page.tsx
+++ app/task/[id]/page.tsx
@@ -1,6 +1,9 @@
+"use client";
 import TaskCard from "@/app/components/TaskCard";
+import { useState } from "react";
 
 export default function TaskPage() {
+  const [id, setId] = useState("");
   return (
     <main
       className="flex flex-col items-center
