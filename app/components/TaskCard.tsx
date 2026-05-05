0c5fafe30be358b73f1095c3651ff51b2fd5714c
Alperen Sirli <alperensirli99@hotmail.com>
2026-05-05 22:18:41 +0200

    notes text area state save

--- app/components/TaskCard.tsx
+++ app/components/TaskCard.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { useState } from "react";
+import { formattedDate } from "../lib/utils";
 
 export default function TaskCard() {
   const [id, setId] = useState("");
   const [duties, setDuties] = useState<string[]>([]);
   const [dutyInput, setDutyInput] = useState("");
   const [isCompleted, setIsCompleted] = useState(false);
-  const [notes, setNotes] = useState("");
+  const [notes, setNotes] = useState<string[]>([]);
+  const [noteInput, setNoteInput] = useState("");
 
   const handleAddDuty = () => {
     if (!dutyInput.trim()) return;
@@ -14,17 +16,25 @@ export default function TaskCard() {
     setDutyInput("");
   };
 
+  const handleAddNote = () => {
+    if (!noteInput.trim()) return;
+    setNotes([...notes, noteInput.trim()]);
+    setNoteInput("");
+  };
+
   console.log(duties);
+  console.log(notes);
 
   return (
     <main>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center justify-center">
+        {/** Duties input **/}
         <label>What's needs to be done?</label>
         <div className="flex items-center justify-between">
           <input
             type="text"
             placeholder="Presentation e.g."
-            className="w-[300px] py-2 shadow-sm"
+            className="border border-gray-300 rounded-md p-2 w-[300px] shadow-sm focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-transparent"
             value={dutyInput}
             onChange={(e) => setDutyInput(e.target.value)}
             onKeyDown={(e) => e.key === "Enter" && handleAddDuty()}
@@ -35,6 +45,25 @@ export default function TaskCard() {
             onClick={handleAddDuty}
           >
             +Add
+          </button>
+        </div>
+
+        {/** Notes texarea **/}
+        <div className="flex flex-col mt-10 gap-3">
+          <label>Take your note:</label>
+          <textarea
+            placeholder="I should't use phone before sessions to keep my focus sharp."
+            className="border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-transparent"
+            rows={4}
+            value={noteInput}
+            onChange={(e) => setNoteInput(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && handleAddNote()}
+          />
+          <button
+            className="m-auto bg-emerald px-4 py-2 rounded-xl hover:cursor-pointer shrink-0"
+            onClick={handleAddDuty}
+          >
+            Save to notes
           </button>
         </div>
       </div>
